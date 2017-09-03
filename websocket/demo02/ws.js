39f301fa277d6c9e0271bb3d2290514c3d8395c3
sdshaoda <sdshaoda@126.com>
2017-09-03 16:51:56 +0800

    fix and update websocket

--- websocket/demo02/ws.js
+++ websocket/demo02/ws.js
@@ -7,7 +7,6 @@ var server = ws.createServer(function (conn) {
   console.log('New connection')
   conn.on('text', function (str) {
     console.log('Received ' + str)
-    // 转换为大写，并添加三个感叹号
     conn.sendText(str.toUpperCase() + '!!!')
   })
   conn.on('close', function (code, reason) {
@@ -19,4 +18,4 @@ var server = ws.createServer(function (conn) {
   })
 }).listen(PORT)
 
-console.log('websocket server listen on port: ' + PORT)
+console.log('websocket server listen on ws://localhost:' + PORT + '/')
