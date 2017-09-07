adef468d217bec270ea1186abd6c2738bdfc7d7f
sdshaoda <sdshaoda@126.com>
2017-09-07 23:52:40 +0800

    OOP改写

---
+++ Tetris/js/Game.js
@@ -0,0 +1,79 @@
+function Game (doms) {
+  // DOM 元素
+  var gameDiv
+  var nextDiv
+  // 游戏矩阵 10*20
+  var gameData = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+  // 当前方块
+  var cur
+  // 下一个方块
+  var next
+  // divs
+  var nextDivs = []
+  var gameDivs = []
+
+  // 初始化
+  this.init = init
+  this.init(doms)
+
+  function init (doms) {
+    gameDiv = doms.gameDiv
+    nextDiv = doms.nextDiv
+    cur = new Square()
+    next = new Square()
+    initDiv(gameDiv, gameData, gameDivs)
+    initDiv(nextDiv, next.data, nextDivs)
+    refreshDiv(gameData, gameDivs)
+    refreshDiv(next.data, nextDivs)
+  }
+  // 初始化div
+  function initDiv (element, data, divs) {
+    for (var i = 0;i < data.length;i++) {
+      var div = []
+      for (var j = 0;j < data[0].length;j++) {
+        var newNode = document.createElement('div')
+        newNode.className = 'none'
+        newNode.style.top = i * 20 + 'px'
+        newNode.style.left = j * 20 + 'px'
+        element.appendChild(newNode)
+        div.push(newNode)
+      }
+      divs.push(div)
+    }
+  }
+  // 刷新div
+  function refreshDiv (data, divs) {
+    for (var i = 0; i < data.length;i++) {
+      for (var j = 0; j < data[0].length;j++) {
+        if (data[i][j] == 0) {
+          divs[i][j].className = 'none'
+        }else if (data[i][j] == 1) {
+          divs[i][j].className = 'done'
+        }else if (data[i][j] == 2) {
+          divs[i][j].className = 'current'
+        }
+      }
+    }
+  }
+}
---
+++ Tetris/js/Local.js
@@ -0,0 +1,12 @@
+function Local () {
+  // 游戏对象
+  var game
+  // 开始
+  this.start = function () {
+    var doms = {
+      gameDiv: document.querySelector('#game'),
+      nextDiv: document.querySelector('#next')
+    }
+    game = new Game(doms)
+  }
+}
